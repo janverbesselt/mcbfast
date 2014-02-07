7abec963385b98eb5e787205e08ee7500bdc3ded
verbe039 <janverbesselt@gmail.com>
2014-02-07 16:37:35 +0100

    main script update

--- mc_bfm.R
+++ mc_bfm.R
@@ -1,13 +1,11 @@
 ## February 2014
 ## Authors: Jan, Jonathan
 
-
 if (FALSE) {
   ## if needed for mac type = "source"
   install.packages("spatial.tools", 
                    type = "source", repos="http://R-Forge.R-project.org")
 }
-
 
 require("bfast")
 require("spatial.tools")
@@ -16,7 +14,11 @@ require("spatial.tools")
 source("functions.R")
 
 ## load data
-evi <- brick("data/evi_au.grd") ## monthly time series
+if (!file.exists(fn <- "data/evi_au.grd")) {
+  unzip(files="data/evi_au.zip")
+} else {
+  evi <- brick(fn) ## monthly time series
+}
 
 ## test on one time series to demonstrate how bfastmonitor works
 if (FALSE) {
@@ -28,16 +30,28 @@ if (FALSE) {
   plot(fit)
 }
 
-# To run in parallel, uncomment:
-# sfQuickInit(cpu=2)
+## does not work yet --> as we need to build in dealing with NA's in the time series
+## Ben how have you tackled this?
 
-# Now use rasterEngine to execute the function on the brick:
-bfastmonitor_raster <- rasterEngine(rasterTS=evi, 
-  filename=c("data/test.grd"),
-  args=list(start=c(2010,1)), setMinMax = FALSE,
-  fun=bfastmonitor_rasterEngine, debugmode=FALSE)
+if (FALSE) {
+  # To run in parallel, uncomment:
+  if (!file.exists(fn <- "data/test.grd")) {
+    # sfQuickInit(cpu=2)
+    # Now use rasterEngine to execute the function on the brick:
+    bfm_out <- rasterEngine(rasterTS=evi, 
+                      filename=c("data/test.grd"),
+                      args=list(start=c(2010,1)), setMinMax = FALSE,
+                      fun=bfastmonitor_rasterEngine, debugmode=FALSE)
+    # To stop parallel engine, uncomment:
+    # sfQuickStop()
+  }  else {
+    bfm_out <- brick(fn)
+  }
+  plot(bfm_out)
+}
 
-# To stop parallel engine, uncomment:
-# sfQuickStop()
+## no parallel processing
+## to be added the calc based alternative
 
-plot(bfastmonitor_raster)
+
+
